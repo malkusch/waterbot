fd08fbd2936e1ee2d04bbd7f43bb40826e586e3c
Markus Malkusch <markus@malkusch.de>
2017-05-21 15:28:00 +0200

    Analyze data of multiple pots

--- octave/plotMoist.m
+++ octave/plotMoist.m
@@ -1,13 +1,25 @@
-n = 2;
-Data = csvread ("moist.log");
-x = round(Data(:,1)/(1000*60))/60;
-y = Data(:,3);
-p = polyfit(x,y,n);
-f = polyval(p,x);
-d = polyder(p);
-f2 = polyval(d,x);
 
-subplot(2,1,1);
-plot(x,y,'.',x,f,'linewidth',3,'-r');
-subplot(2,1,2);
-plot(x,f2);
+Data = csvread ("moist5.log");
+sensors = max(Data(:,4));
+n = 2;
+sensorColumn = 4;
+dataColumn = 3;
+plotColumns = 2;
+plotRows = sensors;
+
+for sensor = 1:sensors
+
+	A = Data(Data(:,sensorColumn)==sensor,:);
+	x = round(A(:,1)/(1000*60))/60;
+	y = A(:,dataColumn);
+	p = polyfit(x,y,n);
+	f = polyval(p,x);
+	d = polyder(p);
+	f2 = polyval(d,x);
+
+	subplot(plotColumns,plotRows, sensor);
+	plot(x,y,'.',x,f,'linewidth',3,'-r');
+	subplot(plotColumns,plotRows, sensor + sensors);
+	plot(x,f2);
+
+endfor
