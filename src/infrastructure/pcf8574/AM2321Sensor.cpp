be4ef404995391e0da384abbd37b7a7dfca84cf0
Markus Malkusch <markus@malkusch.de>
2017-07-04 20:34:50 +0200

    Log sensor uid
    This should help to confirm that for each pot a different sensor is
    used.

--- src/infrastructure/pcf8574/AM2321Sensor.cpp
+++ src/infrastructure/pcf8574/AM2321Sensor.cpp
@@ -33,6 +33,11 @@ SensorData AM2321Sensor::read() {
 
 	for (byte i = 0; i < AM2321_SENSOR_RETRY_COUNT; i++) {
 		busSwitch->write(State::ON);
+
+		String debug = F("Reading sensor ");
+		debug += String(am2321.uid());
+		Logger::getLogger()->debug(debug);
+
 		if (!am2321.read()) {
 			Logger::getLogger()->warn(F("Failed reading sensor"));
 			continue;
